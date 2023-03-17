a07885a778110d64fe412196a62d8923fdbd8578
sarwar-hsn <sarwar.hossain2016@gtu.edu.tr>
2023-03-17 10:20:09 +0300

    prod-6

--- togroot/production.py
+++ togroot/production.py
@@ -150,7 +150,6 @@ USE_TZ = True
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID') 
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY') 
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME') #ottomangrp
-AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 AWS_S3_ENDPOINT_URL = "https://ams3.digitaloceanspaces.com"
 AWS_S3_CUSTOM_DOMAIN = 'ottomangrp.ams3.cdn.digitaloceanspaces.com'
