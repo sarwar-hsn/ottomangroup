2e11e929aae5ebdf6bb0ec99ab8e3fea5916c159
sarwar-hsn <sarwar.hossain2016@gtu.edu.tr>
2023-04-06 13:48:54 +0300

    texts updated

--- servicesapp/utils/service_utils.py
+++ servicesapp/utils/service_utils.py
@@ -10,44 +10,44 @@ class Service():
 all_services = [
             Service(
                 title="Real Estate & Consultancy", 
-                description="All kinds of services related to buying and selling properties in Turkiye, assist with the process of getting citizenship and make sure the process is complete within the shortest time.",
+                description="All kinds of services related to buying and selling properties in Turkiye.",
                 icon="building.png",
                 color="bg-green",
                 link="servicesapp:servicesapp_real_estate"
                 ),
             Service(
                 title="Citizenship & Residence Permit", 
-                description="Citizenship of services related to buying and selling properties in Turkiye, assist with the process of getting citizenship and make sure the process is complete within the shortest time.",
+                description="Services related to obtaining citizenship including paperwork and consultancy.",
                 icon="passport.png",
                 color="bg-yellow",
                 link="servicesapp:servicesapp_citizenship"),
             Service(
                 title="Export-Import", 
-                description="All kinds of services related to buying and selling properties in Turkiye, assist with the process of getting citizenship and make sure the process is complete within the shortest time.",
+                description="Legal consultancy and advising related to export and import business in Turkiye.",
                 icon="export_import.png",
                 color="bg-purple",
                 link='servicesapp:servicesapp_export_import'),
             Service(
                 title="Study in Turkiye", 
-                description="All kinds of services related to buying and selling properties in Turkiye, assist with the process of getting citizenship and make sure the process is complete within the shortest time.",
+                description="Assisting with the university selection, admission, and payment process in Turkish universities.",
                 icon="study.png",
                 color="bg-pale-blue",
                 link='servicesapp:servicesapp_study'),
             Service(
                 title="Hajj & Umrah", 
-                description="All kinds of services related to buying and selling properties in Turkiye, assist with the process of getting citizenship and make sure the process is complete within the shortest time.",
+                description="Assisting with Hajj and Umrah processing, payments, and travels.",
                 icon="hajj.png",
                 color="bg-pink",
                 link='servicesapp:servicesapp_hajj_umrah'),
             Service(
                 title="Tourism-Health Tourism", 
-                description="All kinds of services related to buying and selling properties in Turkiye, assist with the process of getting citizenship and make sure the process is complete within the shortest time.",
+                description="Arranging tours in different cities of Turkiye with accommodations and transportation services.",
                 icon="tourism.png",
                 color="bg-orange",
                 link='servicesapp:servicesapp_tourism'),
             Service(
                 title="Service Solutions", 
-                description="All kinds of services related to buying and selling properties in Turkiye, assist with the process of getting citizenship and make sure the process is complete within the shortest time.",
+                description="Our expert team is ready to facilitate your work with sincere service.",
                 icon="service.png",
                 color="bg-red",
                 link='servicesapp:servicesapp_service_solutions'),
